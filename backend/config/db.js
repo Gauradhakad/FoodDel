3ae6a9f30b36a673689c83e40221d11e2b6e418f
Gaurav Dhakad <gdgaurav856@gmail.com>
2025-02-01 17:46:30 +0530

    fixed

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,14 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-export const connectDB = async()=>{
-    await mongoose.connect("mongodb+srv://Gaurav:lHvggHeS07sEJkAc@cluster0.vdabi.mongodb.net/FOODDEL").then(()=>console.log("DB Connected"));
-}
+export const connectDB = async () => {
+  try {
+    console.log("Connecting to:", process.env.MONGO_URI); // Debug line to verify the URI
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("DB Connected");
+  } catch (error) {
+    console.error("DB Connection Error:", error);
+    process.exit(1);
+  }
+};
